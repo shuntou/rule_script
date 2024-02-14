912d5e3acf75946f74f5fc84650e7cadcd340a62
Tao <47449015+shuntou@users.noreply.github.com>
2024-02-15 01:57:08 +0800

    Update and rename Xbox CLoud Gaming优化整合.js to Xbox Cloud Gaming Region Unlocker.js

--- module/xbox/Xbox Cloud Gaming Region Unlocker.js
+++ module/xbox/Xbox Cloud Gaming Region Unlocker.js
@@ -1,4 +1,4 @@
 const regex='</title>';const replace_str='</title>\
-<script type="text/javascript" async="async" src="https://update.greasyfork.org/scripts/455741/Xbox%20CLoud%20Gaming优化整合.user.js"></script>\
+<script type="text/javascript" async="async" src="https://update.greasyfork.org/scripts/455261/Xbox%20Cloud%20Gaming%20Region%20Unlocker.user.js"></script>\
 '
 if($response.body){var body=$response.body.replaceAll('</TITLE>','</title>').replaceAll(regex,replace_str)};const headers=$response.headers;headers['Content-Security-Policy']='*';$done({headers:headers,body:body});
